9bad46e1e96967b7d11f2ca45ba2c677648f2d24
aditya <adityapandeyadp@gmail.com>
2023-07-14 00:32:34 +0530

    testinga

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
+batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
-max_iters = 40000
+max_iters = 400000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
+n_embd = 256
+n_head = 32
+n_layer = 32
 dropout = 0.2
 # ------------
 
